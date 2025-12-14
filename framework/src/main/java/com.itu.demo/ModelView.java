7f95e38fc4e8a44c2a06dd42d2d9e93d0596ac7a
IotyRandrianoely <ioty.ratisbonne@gmail.com>
2025-12-14 17:17:34 +0300

    sprint 8

--- framework/src/main/java/com.itu.demo/ModelView.java
+++ framework/src/main/java/com.itu.demo/ModelView.java
@@ -1,13 +1,17 @@
 package com.itu.demo;
 import java.util.HashMap;
 import java.util.Map;
+
 public class ModelView {
     private String view;
     private Map<String, Object> data;
+    
     public ModelView() {
+        this.data = new HashMap<>();
     }
 
     public ModelView(String view) {
+        this();
         this.view = view;
     }
 
@@ -18,23 +22,43 @@ public class ModelView {
     public void setView(String view) {
         this.view = view;
     }
+    
     public Map<String, Object> getData() {
         return data;
-    }   
-    /*
-    @param key
-    @param value
-    */
+    }
+    
+    public void setData(Map<String, Object> data) {
+        this.data = data;
+    }
+    
+    /**
+     * Ajoute un objet dans la map de données
+     */
     public void addObject(String key, Object value) {
         if (data == null) {
             data = new HashMap<>();
         }
         this.data.put(key, value);
     }
+    
+    /**
+     * Sprint 8: Alias de addObject pour correspondre à la convention
+     */
+    public void addData(String key, Object value) {
+        this.addObject(key, value);
+    }
+    
+    /**
+     * Ajoute plusieurs objets à la fois
+     */
+    public void addAllData(Map<String, Object> data) {
+        if (data != null) {
+            this.data.putAll(data);
+        }
+    }
+    
     @Override
     public String toString() {
-        return "ModelView{" +
-                "view='" + view + '\'' +
-                '}';
+        return "ModelView{view='" + view + "', data=" + data + "}";
     }
 }
